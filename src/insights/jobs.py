365bba295010f62dda61afc5e52df5d5691d9ff1
Elivelton <eliveltonmn@outlook.com>
2023-04-27 15:03:07 -0300

    feat:funcao read implementada

--- src/insights/jobs.py
+++ src/insights/jobs.py
@@ -1,22 +1,17 @@
 from functools import lru_cache
 from typing import List, Dict
+import csv
 
 
 @lru_cache
 def read(path: str) -> List[Dict]:
-    """Reads a file from a given path and returns its contents
+    dict = []
+    with open(path) as file:
+        list = csv.DictReader(file)
+        for line in list:
+            dict.append(line)
 
-    Parameters
-    ----------
-    path : str
-        Full path to file
-
-    Returns
-    -------
-    list
-        List of rows as dicts
-    """
-    raise NotImplementedError
+        return dict
 
 
 def get_unique_job_types(path: str) -> List[str]:
@@ -53,6 +48,3 @@ def filter_by_job_type(jobs: List[Dict], job_type: str) -> List[Dict]:
         List of jobs with provided job_type
     """
     raise NotImplementedError
-
-
-# initial_commit
